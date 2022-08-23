0cca39371781d58ab8b77d27180d5c5cbfd38b7e
thibbns <thib.bns@outlook.com>
2022-08-23 17:05:03 +0200

    test suppression

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,6 @@
 
 let item = JSON.parse(localStorage.getItem("item"));
+
 const cartItems = document.getElementById('cart__items');
 
 
@@ -39,14 +40,57 @@ cartItems.innerHTML += `<article class="cart__item" data-id="{product-ID}" data-
         </div>
     </div>
 </article>`
+
+
+function getBasket() {
+    let basket = localStorage.getItem("item");
+    if (basket == null) {
+      return [];
+    } else {
+      return JSON.parse(basket);
+    }
+  }
+  
+  function saveBasket(basket) {
+    localStorage.setItem("item", JSON.stringify(basket));
+  }
+
+
+
+
+
+function removeFromCart(product){
+    let basket = getBasket();
+    basket = basket.filter(p=>p.id != product.id);
+    saveBasket(basket);
+}
+//removeFromCart(item[i])
+
+
+//const removeFromCart = document.getElementsByClassName('deleteItem');
+const removeFromStorage = document.querySelector(".deleteItem");
+removeFromStorage.addEventListener("click", removeFromCart([{id:"415b7cacb65d43b2b5c1ff70f3393ad1"}]));
+
+
+
+
+
+
+
+
 console.log(item.length)
 let totalPrice=0
 for (i=0; i<item.length; i++){
     totalPrice+= Number(item[i].quantity);
 console.log(item.length)
 }
+
+
+
 })
 }
+
+
 
 let totalQuantity=0
 for (i=0; i<item.length; i++){
@@ -54,6 +98,10 @@ for (i=0; i<item.length; i++){
 
     document.getElementById('totalQuantity').innerHTML = totalQuantity
 }
+
+
+
+
 
 
 
