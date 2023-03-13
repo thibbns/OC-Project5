ff21e97dbf5c065475a0276ea180843f92a8dcd2
thibbns <thib.bns@outlook.com>
2023-03-13 11:03:35 +0100

    script.js en inner.HTML

--- front/js/script.js
+++ front/js/script.js
@@ -1,40 +1,26 @@
-function createNode(element) {
-  return document.createElement(element);
-}
-
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
 const section = document.getElementById('items');
 const url = 'http://localhost:3000/api/products';
+const items = document.getElementById('items');
+items.innerHTML = "";
 
 fetch(url)
   .then((resp) => resp.json())
   .then(function (data) {
-    let items = data;
-    return items.map(function (item) {
-      let a = createNode('a');
-      let article = createNode('article');
-      let img = createNode('img');
-      let h3 = createNode('h3');
-      let p = createNode('p');
-      let h4 = createNode('h4');
 
-      a.href = "./product.html?id=" + item._id;
-      img.src = item.imageUrl;
-      img.alt = item.altTxt;
-      h3.innerHTML = item.name;
-      h4.innerHTML = item.price + " €";
-      p.innerHTML = item.description;
+    for (i = 0; i < data.length; i++) {
+      let id = data[i]._id;
+      let image = data[i].imageUrl;
+      let name = data[i].name;
+      let description = data[i].description;
 
-      append(article, img);
-      append(article, h3);
-      append(section, a);
-      append(article, p);
-      append(article, h4);
-      append(a, article);
-    })
+      items.innerHTML += `<a href="./product.html?id=${id}">
+      <article>
+        <img src=${image} alt="Lorem ipsum dolor sit amet, Kanap name1">
+        <h3 class="productName">${name}</h3>
+                <p class="productDescription">${description}</p>
+           </article>
+    </a>`
+    }
   })
 
   .catch(function (error) {
